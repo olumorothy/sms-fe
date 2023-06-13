554b20259a9b3b5a747c9e5bd03c3a5d631e0487
Olumorothy <iseoluwa89@gmail.com>
2023-06-13 10:57:38 +0100

    creates file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Verification from "./pages/Verification";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/verify",
+    element: <Verification />,
+  },
+]);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
   );
 }
 
